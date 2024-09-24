14aeb60f1d55bdc684524bc38328a52129e73e7d
William0Holden <124404125+William0Holden@users.noreply.github.com>
2024-09-23 22:53:11 -0400

    added links into AdminHome

--- frontend-cebs/src/components/admin/AdminHome.js
+++ frontend-cebs/src/components/admin/AdminHome.js
@@ -3,25 +3,35 @@ import React from 'react';
 import './AdminHome.css';
 
 const AdminHome = (props) => {
+  const manageMovies = () => {
+    window.location.href = '/ManageMovies';
+  };
+  const manageUsers = () => {
+    window.location.href = '/ManageUsers';
+  };
+  const managePromotions = () => {
+    window.location.href = '/ManagePromos';
+  };
+
   return (
-    <div class="admin-container">
-        <div class="admin-option">
-            <label for="movieImageUrl">Manage Movies</label>
-            <input type="text" id="movieImageUrl" class="hidden" value="https://png.pngtree.com/png-clipart/20210309/original/pngtree-movie-clip-art-movie-film-field-clapper-board-png-image_5862049.jpg"/>
-            <button onclick="manageMovies()">Manage Movies</button>
+    <div className="admin-container">
+        <div className="admin-option">
+            <label htmlFor="movieImageUrl">Manage Movies</label>
+            <input type="text" id="movieImageUrl" className="hidden" value="https://png.pngtree.com/png-clipart/20210309/original/pngtree-movie-clip-art-movie-film-field-clapper-board-png-image_5862049.jpg"/>
+            <button onClick={manageMovies}>Manage Movies</button>
         </div>
-        <div class="admin-option">
-            <label for="userImageUrl">Manage Users</label>
-            <input type="text" id="userImageUrl" class="hidden" value="https://cdn-icons-png.flaticon.com/512/33/33308.png"/>
-            <button >Manage Users</button>
+        <div className="admin-option">
+            <label htmlFor="userImageUrl">Manage Users</label>
+            <input type="text" id="userImageUrl" className="hidden" value="https://cdn-icons-png.flaticon.com/512/33/33308.png"/>
+            <button onClick={manageUsers}>Manage Users</button>
         </div>
-        <div class="admin-option">
-            <label for="promotionImageUrl">Manage Promotions</label>
-            <input type="text" id="promotionImageUrl" class="hidden" value="https://img.freepik.com/free-vector/marketing-promo-clipart-illustrated_52683-74351.jpg"/>
-            <button >Manage Promotions</button>
+        <div className="admin-option"> 
+            <label htmlFor="promotionImageUrl">Manage Promotions</label>
+            <input type="text" id="promotionImageUrl" className="hidden" value="https://img.freepik.com/free-vector/marketing-promo-clipart-illustrated_52683-74351.jpg"/>
+            <button onClick={managePromotions}>Manage Promotions</button>
         </div>
     </div>
-  )
+  );
 }
 
 export default AdminHome;
