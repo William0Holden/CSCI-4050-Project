e5e5b1801a2b730c96c9da8f1d5dfb86677494ee
William0Holden <124404125+William0Holden@users.noreply.github.com>
2024-09-24 14:06:01 -0400

    Fixed some css and added temporarily working search bar

---
+++ frontend-cebs/src/components/SearchBar.js
@@ -0,0 +1,29 @@
+// Not used in deliverable
+
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+    const [query, setQuery] = useState('');
+
+    const handleInputChange = (e) => {
+        setQuery(e.target.value);
+    };
+
+    const handleSearch = () => {
+        onSearch(query);
+    };
+
+    return (
+        <div>
+            <input 
+                type="text" 
+                value={query} 
+                onChange={handleInputChange} 
+                placeholder="Search for movies..." 
+            />
+            <button onClick={handleSearch}>Search</button>
+        </div>
+    );
+};
+
+export default SearchBar;
