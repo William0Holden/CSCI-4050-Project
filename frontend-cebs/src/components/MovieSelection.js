9fa41bdfeb412569394429bfd78bec21b0a1f792
William0Holden <124404125+William0Holden@users.noreply.github.com>
2024-09-24 14:06:01 -0400

    Fixed some css and added temporarily working search bar

--- frontend-cebs/src/components/MovieSelection.js
+++ frontend-cebs/src/components/MovieSelection.js
@@ -2,16 +2,50 @@ import Movie from './Movie';
 import React, { useState } from 'react';
 import TrailerModal from './TrailerModal';
 
+
+
 //look at backend/backend/movie/models.py for the fields
 
 
 const MovieSelection = (props) => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const Navbar = () => {
+        return (
+            <nav className="navbar">
+                <ul>
+                    <li><a href="/login">Log-In</a></li>
+                    <li><a href="/register">Register</a></li>
+                    <li><a href="/checkout">Checkout</a></li>
+                    <li><a href="/admin">Admin</a></li>
+                </ul>
+            </nav>
+        );
+    };
+
+    const filteredMovies = props.movieData.filter((movie) =>
+        movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
-        <div class="movie-selection">
-            <h1>Select a Movie</h1>
+        <div>
+            <Navbar />
+            <div className="movie-selection">
+                <h1>Select a Movie</h1>
+                
+                <input
+                    type="text"
+                    placeholder="Search for a movie..."
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                />
 
                 <ul>
-                    {props.movieData && props.movieData.map((movie) => (
+                    {filteredMovies && filteredMovies.map((movie) => (
                         <Movie
                             key={movie.id}
                             title={movie.title}
@@ -22,7 +56,7 @@ const MovieSelection = (props) => {
                         />
                     ))}
                 </ul>
-
+            </div>
         </div>
     );
 };
