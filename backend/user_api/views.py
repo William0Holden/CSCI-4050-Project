a53415f558d51c60786ddb37bf6cd7a332ee9b90
William0Holden <124404125+William0Holden@users.noreply.github.com>
2024-10-17 19:53:21 -0400

    addded email confirmation (need to test if its works)

--- backend/user_api/views.py
+++ backend/user_api/views.py
@@ -5,6 +5,26 @@ from rest_framework.response import Response
 from .serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer
 from rest_framework import permissions, status
 from .validations import custom_validation, validate_email, validate_password
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.core.mail import EmailMessage
+
+def email_verification(request, user):
+	current_site = get_current_site(request)
+	mail_subject = 'Activate your account'
+	message = render_to_string('user_api/acc_active_email.html', {
+		'user': user,
+		'domain': current_site.domain,
+		'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+		'token': user.token,
+	})
+	to_email = user.email
+	email = EmailMessage(
+		mail_subject, message, to=[to_email]
+	)
+	email.send()
 
 
 class UserRegister(APIView):
@@ -15,6 +35,7 @@ class UserRegister(APIView):
 		if serializer.is_valid(raise_exception=True):
 			user = serializer.create(clean_data)
 			if user:
+				email_verification(request, user)
 				return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(status=status.HTTP_400_BAD_REQUEST)
 
